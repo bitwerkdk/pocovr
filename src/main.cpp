98c25a7f4ebba75506b1d9069bb07a82cab15143
brian@bitwerk.dk <brian@bitwerk.dk>
2021-01-08 07:22:19 +0100

    Render sprite on dual displays

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,117 @@
-/*
-In the TFT_ESPI user_setup.h file
-be sure to comment out the line for TFT_CS
-//#define TFT_CS   21  // Chip select control pin
-*/
-
+// TFT_eSPI library config: /.pio/libdeps/esp-wrover-kit/TFT_eSPI/User_Setup.h
 #include <SPI.h>
 #include <TFT_eSPI.h> // Hardware-specific library
 
 #define firstScreenCS 21
 #define secondScreenCS 22
-TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
 
+// Set delay after plotting the sprite
+#define DELAY 1000
+
+// Width and height of sprite
+#define WIDTH  128
+#define HEIGHT 128
+
+TFT_eSPI tft = TFT_eSPI();
+TFT_eSprite spr = TFT_eSprite(&tft);
 
 void setup() {
   pinMode(firstScreenCS, OUTPUT);
-  digitalWrite(firstScreenCS, HIGH);
-  
   pinMode(secondScreenCS, OUTPUT);
-  digitalWrite(secondScreenCS, HIGH);
-  
 
-  // We need to 'init' both displays
-  // at the same time. so set both cs pins low
+  // Initialize both displays
   digitalWrite(firstScreenCS, LOW);
   digitalWrite(secondScreenCS, LOW);
   
   tft.init();
-  tft.setTextSize(2);
-  
-  // Set both cs pins HIGH, or 'inactive'
-  digitalWrite(firstScreenCS, HIGH);
-  digitalWrite(secondScreenCS, HIGH);
-
-
-
-  /*
-   Update the first screen
-   all 'tft.' call must be done
-   after setting the cs pin low
-  */
-  digitalWrite(firstScreenCS, LOW);
-  tft.setRotation(1);  
-  tft.fillScreen(TFT_RED);
-  tft.setCursor(0,10);
-  tft.print("Hello World.\nI'm display #1");
-  // set the cs pin back to HIGH when finished
-  // with this particular display
-  digitalWrite(firstScreenCS, HIGH);
-
-
-  // Update the second screen
-  digitalWrite(secondScreenCS, LOW);
   tft.setRotation(2);  
-  tft.fillScreen(TFT_GREEN);
-  tft.setCursor(0,10);
-  tft.print("Hello World.\nI'm display #2");
-  digitalWrite(secondScreenCS, HIGH);
-
+  tft.fillScreen(TFT_BLACK);
   
+  // Set both displays as 'inactive'
+  // digitalWrite(firstScreenCS, HIGH);
+  // digitalWrite(secondScreenCS, HIGH);
+
+  spr.createSprite(WIDTH, HEIGHT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  delay(100);
+void loop(void) {
+  // Fill the whole sprite with black (Sprite is in memory so not visible yet)
+  spr.fillSprite(TFT_BLACK);
+
+  // Number of pixels to draw
+  uint16_t n = 100;
+
+  // Draw 100 random colour pixels at random positions in sprite
+  while (n--)
+  {
+    uint16_t colour = random(0x10000); // Returns colour 0 - 0xFFFF
+    int16_t x = random(WIDTH);        // Random x coordinate
+    int16_t y = random(HEIGHT);       // Random y coordinate
+    spr.drawPixel( x, y, colour);      // Draw pixel in sprite
+  }
+
+  // Draw some lines
+  spr.drawLine(1, 0, WIDTH, HEIGHT-1, TFT_GREEN);
+  spr.drawLine(0, 0, WIDTH, HEIGHT, TFT_GREEN);
+  spr.drawLine(0, 1, WIDTH-1, HEIGHT, TFT_GREEN);
+  spr.drawLine(0, HEIGHT-1, WIDTH-1, 0, TFT_RED);
+  spr.drawLine(0, HEIGHT, WIDTH, 0, TFT_RED);
+  spr.drawLine(1, HEIGHT, WIDTH, 1, TFT_RED);
+
+  // Draw some text with Middle Centre datum
+  spr.setTextDatum(MC_DATUM);
+  spr.drawString("Sprite", WIDTH / 2, HEIGHT / 2, 4);
+
+  // Now push the sprite to the TFT at position 0,0 on screen
+  spr.pushSprite(-40, -40);
+  spr.pushSprite(tft.width() / 2 - WIDTH / 2, tft.height() / 2 - HEIGHT / 2);
+  spr.pushSprite(tft.width() - WIDTH + 40, tft.height() - HEIGHT + 40);
+
+  delay(DELAY);
+
+  // Fill TFT screen with blue
+  tft.fillScreen(TFT_BLUE);
+
+  // Draw a blue rectangle in sprite so when we move it 1 pixel it does not leave a trail
+  // on the blue screen background
+  spr.drawRect(0, 0, WIDTH, HEIGHT, TFT_BLUE);
+
+  int x = tft.width() / 2  -  WIDTH / 2;
+  int y = tft.height() / 2 - HEIGHT / 2;
+
+  uint32_t updateTime = 0;       // time for next update
+
+  while (true)
+  {
+    // Random movement direction
+    int dx = 1; if (random(2)) dx = -1;
+    int dy = 1; if (random(2)) dy = -1;
+
+    // Pull it back onto screen if it wanders off
+    if (x < -WIDTH/2) dx = 1;
+    if (x >= tft.width()-WIDTH/2) dx = -1;
+    if (y < -HEIGHT/2) dy = 1;
+    if (y >= tft.height()-HEIGHT/2) dy = -1;
+
+    // Draw it 50 time, moving in random direct or staying still
+    n = 50;
+    int wait = random (50);
+    while (n)
+    {
+      if (updateTime <= millis())
+      {
+        // Use time delay so sprtie does not move fast when not all on screen
+        updateTime = millis() + wait;
+
+        // Push the sprite to the TFT screen
+        spr.pushSprite(x, y);
+
+        // Change coord for next loop
+        x += dx;
+        y += dy;
+        n--;
+        yield(); // Stop watchdog reset
+      }
+    }
+  } // Infinite while, will not exit!
 }
