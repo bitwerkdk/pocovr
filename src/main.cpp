34159ac599b18c1f3191071526dd44608ff8d203
brian@bitwerk.dk <brian@bitwerk.dk>
2021-01-09 20:02:11 +0100

    stupi manual save!

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include <Arduino.h>
-#include "macros.h"
-#include "display.h"
 
 #define firstScreenCS 21
 #define secondScreenCS 22
-
-uint16_t* colorBuffer;
-
-int x, y;
 
 void setup() {
   Serial.begin(115200);
@@ -19,22 +13,9 @@ void setup() {
   digitalWrite(firstScreenCS, LOW);
   digitalWrite(secondScreenCS, LOW);
 
-  colorBuffer = (uint16_t*)malloc(LCD_WIDTH * LCD_HEIGHT * LCD_DEPTH);
-  Serial.println("Before initializing");
-  Odroid_InitializeDisplay();
-  Serial.println("After initializing");
+  Serial.println("setup");
 }
 
 void loop() {
-  Serial.println("Start of loop");
-  //memset(colorBuffer, 0x00, LCD_WIDTH * LCD_HEIGHT * LCD_DEPTH);
-  /*for (int row = y; row < y + 50; ++row)
-	{
-		for (int col = x; col < x + 50; ++col)
-		{
-			colorBuffer[LCD_WIDTH * row + col] = 0xF800;
-		}
-	}*/
-  Odroid_DrawFrame(colorBuffer);
-  Serial.println("End of loop");
+  Serial.println("loop");
 }
